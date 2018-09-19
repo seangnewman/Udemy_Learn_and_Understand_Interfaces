24dba1001dbb9a40a13d4fd50722f58a98bc9dfb
Sean Newman <seangnewman@msn.com>
2018-09-19 13:19:02 -0700

    Initial commit

---
+++ Udemy_Learn_and_Understand_Interfaces/IDrawable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace Udemy_Learn_and_Understand_Interfaces
+{
+    interface IDrawable:IComparable, ICloneable
+    {
+        int Width { get; set; }
+        int Height { get; set; }
+        Rectangle Bounds { get; }
+        bool isHighlighted { set; }
+
+    }
+}
---
+++ Udemy_Learn_and_Understand_Interfaces/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy_Learn_and_Understand_Interfaces
+{
+    class Test
+    {
+        interface ITest
+        {
+
+        }
+    }
+}
